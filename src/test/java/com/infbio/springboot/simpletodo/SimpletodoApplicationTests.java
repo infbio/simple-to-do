1026803c0a5742edbf3cb5dc564c3e0948f0411e
최준혁 <hanjan1290@gmail.com>
2024-10-15 18:04:52 +0900

    first commit: build

---
+++ src/test/java/com/infbio/springboot/simpletodo/SimpletodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infbio.springboot.simpletodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpletodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
